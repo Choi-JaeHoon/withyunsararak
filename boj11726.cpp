2fcfc614a9adbc3657571bfc67835c50e9d6547d
yunsarasak <rkddbstjr2@naver.com>
2021-05-03 23:03:11 +0900

    test from yun

--- boj11726.cpp
+++ boj11726.cpp
@@ -1,26 +1,26 @@
 // BOJ 11726
-// 2XN Ÿ�ϸ�
+// 2XN Ÿ�ϸ�
 //
 //
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//int main()
-//{
-//	int n;
-//
-//	cin >> n;
-//
-//	vector<int> v(n);
-//
-//	v[0] = 1;
-//	v[1] = 2;
-//	for (int i = 2; i < n; i++) {
-//		v[i] = (v[i - 2] + v[i - 1]) % 10007;
-//	}
-//
-//	cout << v[n-1];
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	vector<int> v(n);
+
+	v[0] = 1;
+	v[1] = 2;
+	for (int i = 2; i < n; i++) {
+		v[i] = (v[i - 2] + v[i - 1]) % 10007;
+	}
+//gg
+	cout << v[n-1];
+
+	return 0;
+}
