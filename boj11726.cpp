6db6230b578c12e8c495838c54c2673ddff5c54a
hun2 <kwj17711@gmail.com>
2021-05-03 23:02:28 +0900

    jooseok

--- boj11726.cpp
+++ boj11726.cpp
@@ -1,25 +1,26 @@
 // BOJ 11726
 // 2XN ≈∏¿œ∏µ
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main()
-{
-	int n;
-
-	cin >> n;
-
-	vector<int> v(n);
-
-	v[0] = 1;
-	v[1] = 2;
-	for (int i = 2; i < n; i++) {
-		v[i] = (v[i - 2] + v[i - 1]) % 10007;
-	}
-
-	cout << v[n-1];
-
-	return 0;
-}
+//
+//
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//
+//	cin >> n;
+//
+//	vector<int> v(n);
+//
+//	v[0] = 1;
+//	v[1] = 2;
+//	for (int i = 2; i < n; i++) {
+//		v[i] = (v[i - 2] + v[i - 1]) % 10007;
+//	}
+//
+//	cout << v[n-1];
+//
+//	return 0;
+//}
